e9db27e2ecc884aef64c5a9f41beec8f3c12ee29
Jina Park <jinapark119@gmail.com>
2021-04-26 18:30:31 +0100

    hello

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -10,16 +10,21 @@ public class Wall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int row = 0;
         int halfw = width / 2;
-        for(int col = -halfw; col < halfw; col++)
+
+        for(int row = 0; row < height; row++)
         {
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Vector3 p = new Vector3(col, row, 0);
-            go.transform.position = transform.TransformPoint(p);
-            go.transform.rotation = transform.rotation;
-            go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+            for(int col = -halfw; col < halfw; col++)
+            {
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Vector3 p = new Vector3(col, row * 1.1f, 0);
+                go.transform.position = transform.TransformPoint(p);
+                go.transform.rotation = transform.rotation;
+                go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+                go.AddComponent<Rigidbody>();
+            }
         }
+        
     }
 
     // Update is called once per frame
