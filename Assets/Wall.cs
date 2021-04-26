c27f7c98e681510fc5a0c69203f1a0b870dfcc7c
Jina Park <jinapark119@gmail.com>
2021-04-26 18:23:31 +0100

    hello

---
+++ Assets/Wall.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int width = 10; // 10 X 20 wall
+    public int height = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int row = 0;
+        int halfw = width / 2;
+        for(int col = -halfw; col < halfw; col++)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 p = new Vector3(col, row, 0);
+            go.transform.position = transform.TransformPoint(p);
+            go.transform.rotation = transform.rotation;
+            go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
